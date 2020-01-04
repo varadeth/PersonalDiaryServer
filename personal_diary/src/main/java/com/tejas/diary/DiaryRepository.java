495cd13f55a14d6ff0311955fda5143dac88cab9
tej <varadetejas0@gmail.com>
2020-01-04 18:47:49 +0530

    Removed Diary Package

--- personal_diary/src/main/java/com/tejas/diary/DiaryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tejas.diary;
-
-import java.util.ArrayList;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DiaryRepository extends CrudRepository<DiaryContent,String>{
-	public ArrayList<String> findByUsername(String username);
-}
--- personal_diary/src/main/java/com/tejas/person/PersonRepository.java
+++ personal_diary/src/main/java/com/tejas/person/PersonRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 public interface PersonRepository extends CrudRepository<Person, String> {
 	
 	
-	
 }
