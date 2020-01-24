3711ac5c2caeaa17eaadd5c0ff739f23a86e0c3f
tej <varadetejas0@gmail.com>
2020-01-24 09:59:02 +0530

    Encryption Added

--- personal_diary/src/main/java/com/tejas/diary/DiaryController.java
+++ personal_diary/src/main/java/com/tejas/diary/DiaryController.java
@@ -1,10 +1,19 @@
 package com.tejas.diary;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,17 +26,44 @@ public class DiaryController {
 
 	@Autowired
 	private DiaryService diaryService;
-	
+	Cipher encryptCipher;
+	Cipher decryptCipher;
+	private static byte[] key = {
+            0x74, 0x68, 0x69, 0x73, 0x49, 0x73, 0x41, 0x53, 0x65, 0x63, 0x72, 0x65, 0x74, 0x4b, 0x65, 0x79
+    };
+	SecretKey secretKey;
 	public DiaryController() {
+		try {
+			encryptCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			decryptCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+			secretKey = new SecretKeySpec(key,"AES");
+			encryptCipher.init(Cipher.ENCRYPT_MODE, secretKey);
+			decryptCipher.init(Cipher.DECRYPT_MODE, secretKey);
+		}
+		catch (NoSuchPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
 	@PostMapping("/addContent/{username}")
 	public int addContent(@PathVariable String username,@RequestBody Content content) {
 		System.out.println(content);
 		SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
 		content.setDate(formatter.format(new Date()));
-		Diary diary = new Diary(username,content.getText(),content.getDate());
-		System.out.println(content);
+		String text = content.getText();
+		try {
+			text = Base64.getEncoder().encodeToString(encryptCipher.doFinal(text.getBytes()));
+		} catch (IllegalBlockSizeException | BadPaddingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Diary diary = new Diary(username,text,content.getDate());
 		diaryService.addContent(diary);
 		return 200;
 	}
@@ -48,9 +84,21 @@ public class DiaryController {
 		ArrayList<Content> contentList = new ArrayList<Content>();
 		for(Diary d : list) {
 			if(d.getUsername().equals(username)) {
-				contentList.add(d.getDiaryContent());
+				String encrypted = d.getDiaryContent().getText();
+				String decrypted="";
+				try {
+					decrypted = new String(decryptCipher.doFinal(Base64.getDecoder().decode(encrypted.getBytes())));
+				} catch (IllegalBlockSizeException | BadPaddingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("Decrypted : "+decrypted);
+				Content c = d.getDiaryContent();
+				c.setText(decrypted);
+				contentList.add(c);
 			}
 		}
 		return contentList;
 	}
+	
 }
