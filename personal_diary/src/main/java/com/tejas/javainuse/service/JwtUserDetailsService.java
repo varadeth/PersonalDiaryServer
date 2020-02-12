3661fa29e9ed30f9f1573958de4934c9a23720d2
tej <varadetejas0@gmail.com>
2020-02-13 01:36:04 +0530

    Authentication Token Added while logging in

---
+++ personal_diary/src/main/java/com/tejas/javainuse/service/JwtUserDetailsService.java
@@ -0,0 +1,55 @@
+package com.tejas.javainuse.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.tejas.person.Person;
+import com.tejas.person.PersonRepository;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private PersonRepository personRepository;
+	
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		Person person = personRepository.findByUsername(username);
+		System.out.println(username);
+		if(person == null) {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		} 
+		return new org.springframework.security.core.userdetails.User(person.getUsername(), person.getPassword(),
+				new ArrayList<>());
+	}
+	
+	public int addPerson(Person person) {
+		Person previousPerson = getPerson(person.getUsername());
+		if(previousPerson != null) {
+			return 409;
+		}
+		Person response = personRepository.save(person);
+		if(response != null) {
+			return 200;
+		}
+		return 500;
+	}
+	
+	public Person getPerson(String userName) {
+		Person person = personRepository.findByUsername(userName);
+		return person;
+	}
+	
+
+}
--- personal_diary/src/main/java/com/tejas/person/PersonRepository.java
+++ personal_diary/src/main/java/com/tejas/person/PersonRepository.java
@@ -2,7 +2,9 @@ package com.tejas.person;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends CrudRepository<Person, String> {
 	
 	@Query("select s from Person s where s.username = ?1")
